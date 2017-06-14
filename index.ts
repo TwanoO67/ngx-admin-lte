747a91b49188b47488fba60970a48f2f6192182c
WEBER Antoine <ghostsmaker@hotmail.com>
2017-06-15 01:52:47 +0200

    preparation export module

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
-export { NgxAdminLteModule } from './src/ngx-admin-lte.module';
-/*//services
-export { BreadcrumbService } from './src/services/breadcrumb.service';
-export { CanActivateGuard } from './src/services/can-activate-guard.service';
-export { LoggerService } from './src/services/logger.service';
-export { MessagesService } from './src/services/message.service';
-export { RestService } from './src/services/rest.service';
-export { AdminLTETranslateService } from './src/services/translate.service';
-export { UserService } from './src/services/user.service';
+/*
+//services
+export * from './src/services/breadcrumb.service';
+export * from './src/services/can-activate-guard.service';
+export * from './src/services/logger.service';
+export * from './src/services/message.service';
+export * from './src/services/rest.service';
+export * from './src/services/translate.service';
+export * from './src/services/user.service';
 //layouts
-export { LayoutsAuthComponent } from './src/layouts/auth/auth';
+export * from './src/layouts/auth/auth';
+//models
+export * from './src/models/message';
+export * from './src/models/preferencies';
+export * from './src/models/user';
 */
+export { NgxAdminLteModule } from './src/ngx-admin-lte.module';
