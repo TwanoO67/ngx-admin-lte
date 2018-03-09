507805ff2bb0b8d05ce066046cf0a55613ec5cd4
WEBER Antoine <ghostsmaker@hotmail.com>
2018-03-09 08:44:15 +0100

    deplacement index.ts 2.0.0-beta.1

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,27 @@
+"use strict";
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-__export(require('./src/services/breadcrumb.service'));
-__export(require('./src/services/can-activate-guard.service'));
-__export(require('./src/services/logger.service'));
-__export(require('./src/services/messages.service'));
-__export(require('./src/services/notifications.service'));
-__export(require('./src/services/rest.service'));
-__export(require('./src/services/translate.service'));
-__export(require('./src/services/user.service'));
-__export(require('./src/services/menu.service'));
-__export(require('./src/services/logo.service'));
-__export(require('./src/services/footer.service'));
-__export(require('./src/services/control-sidebar.service'));
-__export(require('./src/layouts/auth/auth'));
-__export(require('./src/layouts/login/login.component'));
-__export(require('./src/layouts/register/register.component'));
-__export(require('./src/models/message'));
-__export(require('./src/models/preferencies'));
-__export(require('./src/models/user'));
-__export(require('./src/models/notification'));
-var ngx_admin_lte_module_1 = require('./src/ngx-admin-lte.module');
+exports.__esModule = true;
+__export(require("./services/breadcrumb.service"));
+__export(require("./services/can-activate-guard.service"));
+__export(require("./services/logger.service"));
+__export(require("./services/messages.service"));
+__export(require("./services/notifications.service"));
+__export(require("./services/rest.service"));
+__export(require("./services/translate.service"));
+__export(require("./services/user.service"));
+__export(require("./services/menu.service"));
+__export(require("./services/logo.service"));
+__export(require("./services/footer.service"));
+__export(require("./services/control-sidebar.service"));
+__export(require("./layouts/auth/auth"));
+__export(require("./layouts/login/login.component"));
+__export(require("./layouts/register/register.component"));
+__export(require("./models/message"));
+__export(require("./models/preferencies"));
+__export(require("./models/user"));
+__export(require("./models/notification"));
+var ngx_admin_lte_module_1 = require("./ngx-admin-lte.module");
 exports.NgxAdminLteModule = ngx_admin_lte_module_1.NgxAdminLteModule;
 //# sourceMappingURL=index.js.map
